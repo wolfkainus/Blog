5e10a60d14c420308897b64269a31a45c9716e86
Alfredo Lobos <alfredo.lobos.g@gmail.com>
2015-10-23 01:24:36 -0300

    Add validate to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,10 @@
 class Post < ActiveRecord::Base
+	belongs_to :comments
 	has_many :comments, dependent: :destroy
+
+	#has_many :post_styles
+	#has_many :styles, through:  :post_styles
+
+	validates :title, presence: true
+	validates :content, presence: true
 end
